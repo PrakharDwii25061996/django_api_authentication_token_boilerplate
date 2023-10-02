524951a923e81f44c719505d37c220f68d3709a7
Prakhar Dwivedi <prakhar@gmail.com>
2023-10-02 17:57:47 +0530

    Refactor Project

--- django_accounts/settings.py
+++ django_accounts/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,11 +76,22 @@ WSGI_APPLICATION = 'django_accounts.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': config("DATABASE_NAME"),
+       'USER': config('DATABASE_USER'),
+       'PASSWORD': config('DATABASE_PASSWORD'),
+       'HOST': config('DATABASE_HOST'),
+       'PORT': config('DATABASE_PORT'),
+   }
 }
 
 
