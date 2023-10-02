9a50f9c43266d596155d40d575d841d9799f29e5
Prakhar Dwivedi <prakhar@gmail.com>
2023-10-02 17:44:55 +0530

    Refactored Code

---
+++ accounts/serializers.py
@@ -0,0 +1,79 @@
+""" accounts/serializers.py """
+from rest_framework import serializers
+from accounts.models import CustomUser
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.password_validation import validate_password
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(**validated_data)
+
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=255)
+    password = serializers.CharField(max_length=255)
+
+
+class UserAuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=255)
+    password = serializers.CharField(max_length=255)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if email and password:
+            user = authenticate(request=self.context.get('request'),
+                                email=email, password=password)
+
+            # The authenticate call simply returns None for is_active=False
+            # users. (Assuming the default ModelBackend authentication
+            # backend.)
+            if not user:
+                msg = _('Unable to log in with provided credentials.')
+                raise serializers.ValidationError(msg, code='authorization')
+        else:
+            msg = _('Must include "username" and "password".')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
+
+
+class ChangePasswordSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+    old_password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['password', 'password2', 'old_password']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({'password': "Password fields didn't match"})
+        return attrs
+
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError({'old_password': 'Old Password is not correct'})
+        return value
+
+    def update(self, instance, validated_data):
+        instance.set_password(validated_data['password'])
+        instance.save()
+        return instance
+
+
+class MessageSerializer(serializers.Serializer):
+    message = serializers.CharField(required=False)
---
+++ accounts/views.py
@@ -0,0 +1,81 @@
+""" accounts/views.py """
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.shortcuts import render, HttpResponse, get_object_or_404
+
+from accounts.models import CustomUser
+from accounts.serializers import (
+    CustomUserSerializer, UserAuthTokenSerializer,
+    ChangePasswordSerializer, MessageSerializer
+)
+
+
+class CustomUserAPIView(generics.CreateAPIView):
+    """
+    API through which any user can register
+    """
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        serializer = CustomUserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomLoginAPIView(ObtainAuthToken):
+    """
+    API through which registered user can login
+    """
+    serializer_class = UserAuthTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserAuthTokenSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.validated_data['user']
+            token, created = Token.objects.get_or_create(user=user)
+            response_data = {
+                'email' : user.email,
+                'token' : token.key
+            }
+            return Response(response_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ChangePasswordAPIView(generics.UpdateAPIView):
+
+    queryset = CustomUser.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ChangePasswordSerializer
+
+    # def get_object(self, request):
+    #     import pdb; pdb.set_trace()
+    #     token = Token.objects.get(key=request.auth.key)
+    #     return token.user
+
+    def update(self, request, *args, **kwargs):
+        user = self.get_object(request)
+
+
+class GetMessageAPIView(generics.RetrieveAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = CustomUserSerializer
+
+    def get_object(self):
+        token = Token.objects.get(key=request.auth.key)
+        return CustomUser.objects.get(id=user_id)
+
+    def get(self, request, *args, **kwargs):
+        user = self.get_object()
+        serializer = CustomUserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
